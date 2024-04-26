fb53b93ff6128f9f8bf636973709cfd76fb51158
NiaGerg <161202338+NiaGerg@users.noreply.github.com>
2024-04-26 19:53:53 +0400

    Update views.py
    create task
    და
    task_list
    დავამატე

--- lms_app/views.py
+++ lms_app/views.py
@@ -2,9 +2,11 @@ from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 from django.contrib import messages
 from django.shortcuts import render, redirect, get_object_or_404
-from lms_app.forms import RegisterForm, CheckRegisteredUserForm
+from lms_app.forms import RegisterForm, CheckRegisteredUserForm, TaskForm
 from django.contrib.auth import login, logout
 from lms_app.models import Student, Lecturer, CustomUser, Subject, Faculty
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
@@ -68,3 +70,26 @@ def subject_selection(request):
     return render(request, 'subject_selection.html', {'subjects': None})
 
 
+@login_required
+def create_task(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.lecturer = request.user
+
+            if task.execution_date < timezone.now().date():
+                form.add_error('execution_date', 'Execution date must be in the future.')
+                return render(request, 'create_task.html', {'form': form})
+
+            task.save()
+            messages.success(request, 'Task created successfully.')
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+    return render(request, 'create_task.html', {'form': form})
+
+
+def task_list(request):
+    tasks = request.user.tasks.all()
+    return render(request, 'task_list.html', {'tasks': tasks})
